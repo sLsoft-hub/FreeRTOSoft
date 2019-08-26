3847a37ac8f60cafc55c0656be895da5baccdbfd
sLsoft-hub <sush.lagwankar@gmail.com>
2019-08-26 23:17:26 +0530

    Added initial files

---
+++ sLsoft-FreeRTOS/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sLsoft-FreeRTOS
+//
+//  Created by SUSHRUT on 26/08/19.
+//  Copyright © 2019 sLsoft. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
